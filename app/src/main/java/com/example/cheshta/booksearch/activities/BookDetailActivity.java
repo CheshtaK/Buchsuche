461faa1e24b1515ddcb616d282e3a6b7a4fc61a8
CheshtaK <32400008+CheshtaK@users.noreply.github.com>
2018-03-18 20:17:31 +0530

    sharing feature added

--- app/src/main/java/com/example/cheshta/booksearch/activities/BookDetailActivity.java
+++ app/src/main/java/com/example/cheshta/booksearch/activities/BookDetailActivity.java
@@ -1,14 +1,22 @@
 package com.example.cheshta.booksearch.activities;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.os.Environment;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.cheshta.booksearch.BuildConfig;
 import com.example.cheshta.booksearch.R;
 import com.example.cheshta.booksearch.client.BookClient;
 import com.example.cheshta.booksearch.model.Book;
@@ -19,10 +27,15 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import cz.msebera.android.httpclient.Header;
 
 public class BookDetailActivity extends AppCompatActivity {
 
+    private static final String TAG ="BOOK" ;
     private ImageView ivBookCover;
     private TextView tvTitle;
     private TextView tvAuthor;
@@ -79,6 +92,61 @@ public class BookDetailActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_book_detail, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_share) {
+            setShareIntent();
+            Log.d(TAG, "onOptionsItemSelected: function call");
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setShareIntent() {
+        ImageView ivImage = findViewById(R.id.ivBookCover);
+        final TextView tvTitle = findViewById(R.id.tvTitle);
+
+        Uri bmpUri = getLocalBitmapUri(ivImage);
+
+        Intent shareIntent = new Intent();
+
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.setType("*/*");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, (String)tvTitle.getText());
+        shareIntent.putExtra(Intent.EXTRA_STREAM, bmpUri);
+        Log.d(TAG, "setShareIntent: starting intent");
+
+        startActivity(Intent.createChooser(shareIntent, "Share Image"));
+    }
+
+    public Uri getLocalBitmapUri(ImageView imageView) {
+        Drawable drawable = imageView.getDrawable();
+        Bitmap bmp = null;
+        if (drawable instanceof BitmapDrawable){
+            bmp = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+        } else {
+            return null;
+        }
+
+        Uri bmpUri = null;
+        Log.d(TAG, "getLocalBitmapUri: Creating file");
+        try {
+            File file =  new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES),
+                    "share_image_" + System.currentTimeMillis() + ".png");
+            file.getParentFile().mkdirs();
+            FileOutputStream out = new FileOutputStream(file);
+            bmp.compress(Bitmap.CompressFormat.PNG, 90, out);
+            out.close();
+            bmpUri = FileProvider.getUriForFile(BookDetailActivity.this, BuildConfig.APPLICATION_ID + ".provider",file);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bmpUri;
     }
 
 }
